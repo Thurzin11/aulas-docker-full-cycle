1fe922337f52972e5e6736850a8173440d3ef191
Arthur <arthursantosgoncalves587@gmail.com>
2025-06-21 14:06:52 -0300

    feat: update Dockerfile and main.go to support dynamic port configuration

--- docker aula/mygo_app/main.go
+++ docker aula/mygo_app/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func main()  {
+func main() {
+
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		fmt.Printf("Server started on port :%s", args[0])
+		http.ListenAndServe(":"+args[0], nil)
+
+	}
+
 	fmt.Println("Starting server on port :8080")
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", nil)
 }
